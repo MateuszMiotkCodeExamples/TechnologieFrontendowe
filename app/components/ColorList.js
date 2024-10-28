07e68541a75d33b55c12ddaf4524acfab7b53dc3
Mateusz Miotk <mmiotk@inf.ug.edu.pl>
2024-10-28 11:45:31 +0100

    Add state managment example

---
+++ app/components/ColorList.js
@@ -0,0 +1,17 @@
+import Color from "@/app/components/Color";
+
+export default function ColorList({colors = [], onRemoveColor = f => f, onRateColor = f => f}) {
+    if(!colors.length) return <div>No colors listed</div>
+    return (
+        <div>
+            {
+                colors.map(color => <Color
+                    key={color.id}
+                    {...color}
+                    onRemove={onRemoveColor}
+                    onRate={onRateColor}
+                />)
+            }
+        </div>
+    )
+}
---
+++ app/components/StarRating.js
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react';
+import {Star} from "@/app/components/Star";
+
+const createArray = length => [...Array(length)]
+
+export default function StarRating({totalStars = 5, selectedStars = 0, onRate = f => f, style, ...props}) {
+    return (
+        <div style={{padding: '5px', ...style}} {...props}>
+            {
+                createArray(totalStars).map((n,i) => (
+                    <Star
+                        key={i}
+                        selected={selectedStars > i}
+                        onSelect={() => onRate(i+1)}
+                    />
+                ))
+            }
+            <p>{selectedStars} of {totalStars} stars</p>
+        </div>
+    )
+}
